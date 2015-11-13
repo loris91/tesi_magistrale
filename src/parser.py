b21e06e998312c10103adb86151890c465a467f2
loris91 <loris.marsico@gmail.com>
2015-11-13 17:27:57 +0100

    Creazione classe per il calcolo dei commenti pros e cons

--- src/parser.py
+++ src/parser.py
@@ -1,27 +1,70 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
 import os
+import re
 import json
+import nltk
 from pprint import pprint
+
 
 review_folder = "/home/alakay/Scrivania/cache/review"
 
-def prova():
-	data = {}
-	data['Username'] = 'Utente_1'
-	data['Category'] = 'Mobile'
-	data['Name'] = 'Name_1'
-	data['Advantages'] = 'a1 a2 a3,a4 a5 a6,a7 a8 a9 10'
-	data['Disadvantages'] = 'd1 d2, d3 d4 d5 d6, d7 d8 d9'
-	json_data = json.dumps(data)
+def pulisci(phrase):
+	#phrase = re.sub(r"&#160;"," ",phrase) #Elimino i non-breaking space
+	#phrase = re.sub(r"&#163;","",phrase) #Elimino il symbol £
+	#phrase = re.sub(r"&#133;"," ",phrase) #Elimino i puntini di sospensione
 
-	with open("/home/alakay/Scrivania/file_json.txt","a") as myfile:
-			myfile.write(str(json_data))
+	phrase = re.sub(r"&[^;]*;"," ",phrase) #Elimino i caratteri speciali
+	phrase = re.sub(r" +"," ",phrase) #Elimino gli spazi ripetuti
+	return phrase
 
-def analizza_file(file_name):
-	file = open(review_folder+"/"+file_name)
-	data = json.load(file)
+def split_phrase(phrase,data):
+	phrase = re.sub(r"[,.!?;]",".",phrase)
+	phrase = phrase.split(".")
+	
+	for opinion in phrase:
+		if nltk.word_tokenize(opinion) != []:
+			data['opinion'].append(opinion)
 
-if __name__ == "__main__":	
+	return data
+
+def analizza_file(file_name,data_pros,data_cons):
+	with open(review_folder+"/"+file_name) as json_file:
+		json_data = json.load(json_file)
+		json_file.close()
+
+	for element in json_data['reviews']:
+		pros = pulisci(element["Advantages"])
+		split_phrase(pros,data_pros)
+
+		cons = pulisci(element["Disadvantages"])
+		split_phrase(cons,data_cons)
+
+	with open ("/home/alakay/Scrivania/cache/pros.json","w") as myfile:
+			json.dump(data_pros, myfile)
+
+	with open ("/home/alakay/Scrivania/cache/cons.json","w") as myfile:
+			json.dump(data_cons, myfile)
+
+
+if __name__ == "__main__":
 	list_files = os.listdir(review_folder)
-	#print list_files[0]
-	#analizza_file(list_files[0])
-	prova()
+
+	data_pros = {}
+	data_pros['opinion'] = []
+	data_cons = {}
+	data_cons['opinion'] = []
+	
+	
+	for product in list_files:
+		analizza_file(product,data_pros,data_cons)
+
+	print len (data_pros['opinion'])
+	print len (data_cons['opinion'])
+
+	with open ("/home/alakay/Scrivania/cache/pros.json","w") as myfile:
+			json.dump(data_pros, myfile)
+
+	with open ("/home/alakay/Scrivania/cache/cons.json","w") as myfile:
+			json.dump(data_cons, myfile)
---
+++ src/prova.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+import nltk
+
+if __name__ == "__main__":
+	'''
+	phrase = "Terrible lead channel . No effects loop . No footswitchable channels !! !"	
+	print phrase
+	print "\n____________________\n"
+
+	tokens = nltk.word_tokenize(phrase)
+	print tokens
+	print "\n____________________\n"
+
+	tagged = nltk.pos_tag(tokens)
+	print tagged
+	print "\n____________________\n"
+
+	entities = nltk.chunk.ne_chunk(tagged)
+	print entities
+	print "\n____________________\n"
+	'''
+
+	array = []
+	print array
+
+	array.append("frase 1")
+	print array
+
+	array.append("frase 2")
+	print array
+
+	array.append("frase 3")
+	print array
+
